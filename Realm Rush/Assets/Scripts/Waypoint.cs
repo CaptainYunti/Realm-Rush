134bb9853bd584fd93d6abe77bdf2328039c393d
Yunti <n.pawcio@gmail.com>
2022-10-01 06:12:33 +0200

    Added path, enemy & towers

--- Realm Rush/Assets/Scripts/Waypoint.cs
+++ Realm Rush/Assets/Scripts/Waypoint.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject towerPrefab;
+    [SerializeField] GameObject towersParent;
+    [SerializeField] bool isPlaceable;
+
+
+    private void OnMouseDown()
     {
-        
+        if(isPlaceable)
+        {
+            Instantiate(towerPrefab, transform.position, Quaternion.identity, towersParent.transform);
+            isPlaceable = false;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
